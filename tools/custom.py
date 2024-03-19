b4ca4c7cb5554e76bd64ba0bd15d859bca91b3a9
Hyunjae Kim <peterhyunjae@naver.com>
2024-03-19 18:59:39 +0900

    pred, 원본 img blending 성공

--- tools/custom.py
+++ tools/custom.py
@@ -12,6 +12,7 @@ import models
 import torch
 import torch.nn.functional as F
 from PIL import Image
+from tqdm import tqdm
 
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
@@ -43,6 +44,7 @@ class_colors = {
     'Vessel': (0, 255, 0),  # 초록색
     'Fat': (255, 255, 0),  # 노란색
     'SoftTissue': (70,130,180),  # 청록색
+    'Dura' : (107, 102, 255), # 연한 보라색
     'Disc': (0, 0, 255),  # 파란색
     'Instrument': (119, 11, 32), # 어두운 자주색
     'Cage':(  0,  0, 70),  # 매우 어두운 파란색
@@ -57,6 +59,7 @@ custom_color_map = [
     (0, 255, 0),  # 초록색
     (255, 255, 0),  # 노란색
     (70,130,180),  # 청록색
+    (107, 102, 255), # 연한 보라색
     (0, 0, 255),  # 파란색
     (119, 11, 32), # 어두운 자주색
     (  0,  0, 70),  # 매우 어두운 파란색
@@ -123,6 +126,20 @@ def load_pretrained(model, pretrained):
     
     return model
 
+# 원본 이미지와 인퍼런스 결과를 혼합하기 위해 알파 블렌딩을 사용
+def blend_image_with_mask(original_img, mask, color_palete, alpha=0.5, gamma=0):
+    # mask는 (height, width)의 2D 배열이고, 각 픽셀의 값은 해당 클래스의 인덱스입니다.
+    # custom_color_map을 사용하여 mask에 색을 적용합니다.
+    beta = 1 - alpha
+    # print(original_img.shape, mask.shape)
+    color_mask = np.zeros_like(original_img)
+    for i, color in enumerate(color_palete):
+        color_mask[mask == i] = color
+    
+    # 원본 이미지와 색상 마스크를 혼합합니다.
+    blended_img = cv2.addWeighted(original_img, beta, color_mask, alpha, gamma)
+    return blended_img
+
 if __name__ == '__main__':
     args = parse_args()
     images_list = glob.glob(args.r+'*'+args.t)
@@ -134,7 +151,7 @@ if __name__ == '__main__':
     width, height = args.s
     if args.custom:
         # construct model
-        model = models.pidnet.get_pred_model(args.a, 11, True)
+        model = models.pidnet.get_pred_model(args.a, 11, True) # 12 로 수정 필요
         # load model pretrained
         model = load_pretrained2(model, args.p).cuda()
     else:
@@ -143,35 +160,35 @@ if __name__ == '__main__':
 
     model.eval()
     with torch.no_grad():
-        for img_path in images_list:
+        for img_path in tqdm(images_list):
             img_name = img_path.split("\\")[-1]
-            img = cv2.imread(os.path.join(args.r, img_name),
+            original_img = cv2.imread(os.path.join(args.r, img_name),
                                cv2.IMREAD_COLOR)
             # image shape change
-            img = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
+            img = cv2.resize(original_img, (width, height), interpolation=cv2.INTER_AREA)
 
             sv_img = np.zeros_like(img).astype(np.uint8)
             img = input_transform(img)
             img = img.transpose((2, 0, 1)).copy()
             img = torch.from_numpy(img).unsqueeze(0).cuda()
             pred = model(img)
-            pred = F.interpolate(pred, size=img.size()[-2:], 
+
+            # pred size 맞춰주기
+            pred = F.interpolate(pred, size=original_img.shape[:2], 
                                  mode='bilinear', align_corners=True)
             pred = torch.argmax(pred, dim=1).squeeze(0).cpu().numpy()
             
             if args.custom:
-                for i, color in enumerate(custom_color_map):
-                    for j in range(3):
-                        sv_img[:,:,j][pred==i] = custom_color_map[i][j]
+                blended_img = blend_image_with_mask(original_img, pred, custom_color_map ,alpha=0.3)
             else:
-                for i, color in enumerate(color_map):
-                    for j in range(3):
-                        sv_img[:,:,j][pred==i] = color_map[i][j]
-            sv_img = Image.fromarray(sv_img)
+                blended_img = blend_image_with_mask(original_img, pred, color_map ,alpha=0.8)
+            
+            # image 저장하기 전 처리
+            blended_img = Image.fromarray(blended_img)
             
             if not os.path.exists(sv_path):
                 os.mkdir(sv_path)
-            sv_img.save(sv_path+img_name)
+            blended_img.save(sv_path+img_name)
             
             
             
