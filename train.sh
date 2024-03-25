e6cde60ecbaccd000e6c70407b99491596d4afdc
Hyunjae Kim <peterhyunjae@naver.com>
2024-03-25 20:07:50 +0900

    새로운 config 에 맞게 업데이트

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 python tools/train.py --cfg configs/endoscope/pidnet_small_endoscope.yaml
-python tools/train.py --cfg configs/endoscope/pidnet_large_endoscope.yaml
+python tools/train.py --cfg configs/endoscope/pidnet_medium_endoscope.yaml
