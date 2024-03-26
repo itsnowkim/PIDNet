576403696788102c63ee5a2cba824f06d2012a37
Hyunjae Kim <peterhyunjae@naver.com>
2024-03-26 18:29:34 +0900

    update train config, gitignore

--- train.sh
+++ train.sh
@@ -1,2 +1,8 @@
+python tools/train.py --cfg configs/endoscope/pidnet_small_endoscope2.yaml
+python tools/train.py --cfg configs/endoscope/pidnet_medium_endoscope2.yaml
+python tools/train.py --cfg configs/endoscope/pidnet_large_endoscope2.yaml
+
 python tools/train.py --cfg configs/endoscope/pidnet_small_endoscope.yaml
 python tools/train.py --cfg configs/endoscope/pidnet_medium_endoscope.yaml
+python tools/train.py --cfg configs/endoscope/pidnet_large_endoscope.yaml
+
